cd8062d4024847a02aa2e8053ab1df219311515a
SunValley <buckovika0@gmail.com>
2024-02-01 18:32:01 +0200

    Добавьте файлы проекта.

---
+++ classTemplates/classTemplates.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+using namespace std;
+
+template<typename T>
+class Swapper
+{
+public:
+    void Swap(T& first, T& second);
+};
+
+template<typename T>
+void Swapper<T>::Swap(T& first, T& second) {
+    T temp;
+    temp = first;
+    first = second;
+    second = temp;
+}
+
+int main()
+{
+    int a = 5;
+    int b = 10;
+    cout << "Before swapping: a = " << a << ", b = " << b << endl;
+    Swapper<int> swapper;
+    swapper.Swap(a, b);
+
+    cout << "After swapping: a = " << a << ", b = " << b << endl;
+
+    return 0;
+}
+
+
